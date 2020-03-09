88e3d70a59870f3a3ae9092a36718aa85eb51262
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2020-03-09 10:22:10 +0100

    Inital commit

---
+++ lib/dart_n26.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_n26;
+
+export 'src/dart_n26_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
